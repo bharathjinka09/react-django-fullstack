5e57729e2122f3338bd2690a9c54dc85d5a59297
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-12 13:29:28 +0530

    Redux and HTTP

--- frontend/src/actions/leads.js
+++ frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 // GET Leads
 export const getLeads = () => (dispatch) => {
@@ -17,14 +17,31 @@ export const getLeads = () => (dispatch) => {
 
 // DELETE LEAD
 export const deleteLead = (id) => (dispatch) => {
-    axios
-      .delete(`/api/leads/${id}/`)
-      .then((res) => {
-        // dispatch(createMessage({ deleteLead: 'Lead Deleted' }));
-        dispatch({
-          type: DELETE_LEAD,
-          payload: id,
-        });
-      })
-      .catch((err) => console.log(err));
-  };
+  axios
+    .delete(`/api/leads/${id}/`)
+    .then((res) => {
+      // dispatch(createMessage({ deleteLead: 'Lead Deleted' }));
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+// ADD LEAD
+export const addLead = (lead) => (dispatch) => {
+  axios
+    .post("/api/leads/", lead)
+    .then((res) => {
+      // dispatch(createMessage({ addLead: "Lead Added" }));
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data,
+      });
+    })
+    .catch(
+      (err) => console.log(err)
+      // dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
